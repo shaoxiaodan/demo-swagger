84589b6821e80b0a4561755ed9efe5620be57006
shaoxiaodan <sandays_cn@163.com>
2024-07-26 12:04:47 -0700

    Update UserController.java

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -7,13 +7,30 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.dao.User;
 import com.example.demo.utils.JsonData;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
 @RestController
 @RequestMapping("user")
+@Api(tags = "用户模块", value = "用户controller")
 public class UserController {
 
+	@ApiOperation("用户注册")
 	@GetMapping("register")
 	public JsonData register(User registerUser) {
 		return JsonData.buildSuccess();
 	}
 
+	@ApiOperation("用户列表")
+	@GetMapping("list")
+	public JsonData list() {
+		return JsonData.buildSuccess();
+	}
+
+	@ApiOperation("用户查询")
+	@GetMapping("query")
+	public JsonData query(@ApiParam(name = "userName", value = "用户名称", example = "Java") String userName) {
+		return JsonData.buildSuccess();
+	}
 }
