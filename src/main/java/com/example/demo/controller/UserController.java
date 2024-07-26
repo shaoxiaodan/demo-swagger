c9f25e74a9aa51d598c04e30dda52f6350e6fe5f
shaoxiaodan <sandays_cn@163.com>
2024-07-26 12:15:50 -0700

    Update UserController.java

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +14,7 @@ import com.example.demo.utils.JsonData;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
 @RequestMapping("user")
@@ -17,20 +22,50 @@ import io.swagger.annotations.ApiParam;
 public class UserController {
 
 	@ApiOperation("用户注册")
-	@GetMapping("register")
+	@GetMapping("/reg")
 	public JsonData register(User registerUser) {
 		return JsonData.buildSuccess();
 	}
 
 	@ApiOperation("用户列表")
-	@GetMapping("list")
+	@GetMapping("/list")
 	public JsonData list() {
 		return JsonData.buildSuccess();
 	}
 
 	@ApiOperation("用户查询")
-	@GetMapping("query")
+	@GetMapping("/query")
 	public JsonData query(@ApiParam(name = "userName", value = "用户名称", example = "Java") String userName) {
 		return JsonData.buildSuccess();
 	}
+
+	@ApiOperation("用户添加")
+	@PostMapping("/add")
+	public JsonData addUser() {
+		return JsonData.buildSuccess();
+	}
+
+	/**
+	 * RESTFul风格的API
+	 * 
+	 * @return
+	 */
+	@ApiOperation("用户更新")
+	@PatchMapping("/update/{id}")
+	public JsonData updateById(@ApiParam(name = "id", value = "用户id", example = "1") @PathVariable int id) {
+		return JsonData.buildSuccess();
+	}
+
+	/**
+	 * RESTFul风格的API
+	 * 
+	 * @return
+	 */
+	@ApiIgnore
+	@ApiOperation("用户删除")
+	@DeleteMapping("/remove/{id}")
+	public JsonData removeById(@ApiParam(name = "id", value = "用户id", example = "1") @PathVariable int id) {
+		return JsonData.buildSuccess();
+	}
+
 }
